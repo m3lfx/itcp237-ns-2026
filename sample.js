c0f7aaff64ca53f60bcf94bbfef14538c49d41d2
rommel dalisay <rommelmdalisay@gmail.com>
2026-04-29 09:25:06 +0800

    js

---
+++ sample.js
@@ -0,0 +1,11 @@
+console.log('hello world')
+var today = new Date();
+console.log(today.getDate()); //4
+console.log(today.getMonth()); //
+console.log(today.getFullYear()); //2020
+console.log(today.getHours()); //23
+console.log(today.getMinutes()); //13
+console.log(today.getSeconds()); //10
+//number of milliseconds since January 1, 1970, 00:00:00 UTC
+console.log(today.getTime()); //1432748611392
+console.log(today.getTimezoneOffset()); //-330 Minutes
